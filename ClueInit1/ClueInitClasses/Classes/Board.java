14c8fc7cc99f75e5d063247e4e4574684a868267
Senya Stein <senyarocks11@gmail.com>
2020-10-08 18:06:22 -0600

    Created classes and began to add basics to Board.java

---
+++ ClueInit1/ClueInitClasses/Classes/Board.java
@@ -0,0 +1,28 @@
+package Classes;
+import java.util.*;
+
+public class Board {
+	
+	grid BoardCell[][];
+	int numRows;
+	int numColumns;
+	String layoutConfigFile;
+	String setupConfigFile;
+	Map<Character, Room> roomMap;
+	static Board theInstance;
+	
+	private void intitialize() {
+		
+	}
+	private void loadConfigFiles() {
+		
+	}
+	private void loadSetupConfig() {
+		
+	}
+	private void loadLayoutConfig() {
+		
+	}
+	
+	
+}
