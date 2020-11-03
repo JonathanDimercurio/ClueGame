29c395bdfe0cd81ed4785dc7f8614fb8e017a411
Jonathan Dimercurio <70610851+JonathanDimercurio@users.noreply.github.com>
2020-11-02 19:58:27 -0700

    Solution class created

---
+++ src/clueGame/Solution.java
@@ -0,0 +1,15 @@
+/* Solution
+ * Purpose: This class will contain the solution to the game.
+ * Dependencies:	CardType Enum;
+ * 
+ * @author Jonathan Dimercurio
+ * @author Senya Stein
+ */
+
+package clueGame;
+
+public class Solution {
+	CardType person;
+	CardType room;
+	CardType weapon;
+}
