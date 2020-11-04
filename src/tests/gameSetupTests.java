df771d5099ff3a2e122ba92c12ac7815375a035f
Senya Stein <senyarocks11@gmail.com>
2020-11-04 12:45:20 -0700

    created test class for players and cards

---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clueGame.*;
+
+public class gameSetupTests {
+	//Constants to use for testing
+	
+	
+	//load people and weapons from cluesetup.txt
+	
+	
+	//Create Player class with human and computer child classes.   Use people data to instantiate 6 players (1 human and 5 computer)
+	
+	
+	//Create complete deck of cards (weapons, people and rooms)
+	
+	
+	//Deal cards to the Answer and the players (all cards dealt, players have roughly same # of cards, no card dealt twice) 
+
+}
