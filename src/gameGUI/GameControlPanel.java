c441b6ca08c25b7d117c57079129105189dc017f
Senya Stein <senyarocks11@gmail.com>
2020-11-09 18:48:59 -0700

    Added to GUI program

--- src/gameGUI/GameControlPanel.java
+++ src/gameGUI/GameControlPanel.java
@@ -8,10 +8,26 @@ public class GameControlPanel extends JFrame{
 		setSize(new Dimension(400, 250));
 		setTitle("Neat Stuff Area");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel whoseTurn = new JLabel("Whose turn?");
+		JLabel roll = new JLabel("Roll:");
+		
+		
 	}
 	
 	public static void drawGCP() {
 		GameControlPanel gcp = new GameControlPanel();
 		gcp.setVisible(true);
+		
+	}
+	
+	private JPanel playerNamePanel() {
+		JPanel playerName = new JPanel();
+		playerName.setLayout(new GridLayout(1,1));
+		JLabel playerNameLabel = new JLabel(currentPlayerTurn());
+		panel.add
+	}
+	
+	public void currentPlayerTurn() {
+		
 	}
 }
