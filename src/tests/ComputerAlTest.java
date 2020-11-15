cf09a7bf4f48086d5c38ac9d1da797a2d8bfe6a8
Jonathan Dimercurio <70610851+JonathanDimercurio@users.noreply.github.com>
2020-11-15 15:28:49 -0700

    AlltestsPassing

--- src/tests/ComputerAlTest.java
+++ src/tests/ComputerAlTest.java
@@ -1,89 +1,83 @@
-//package tests;
-//
-//import static org.junit.Assert.*;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//import java.util.Vector;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import clueGame.*;
-//
-//public class ComputerAlTest {
-//	static Board board;
-//	
-//	//We'll need these for all the following tests.
-//	@BeforeAll
-//	public static void setUp() {
-//		// Board is singleton, get the only instance
-//		board = Board.getInstance();
-//		// set the file names to use my config files
-//		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		
-//		// Initialize will load config files 
-//		board.initialize();
-//	}
-//	
-//	//
-//	@Test
-//	public void testSuggestedRoomMatchesCurrentLocartion() {
-//		List<Player> testingPlayerList = new Vector<Player>();
-//		List<Card> testSuggestionList	= new Vector<Card>();
-//
-//		
-//		
-//		
-//		//Move all computer players into rooms, and test
-//		for (ComputerPlayer movePlayer: testingPlayerList) {
-//			while(!movePlayer.getCurrentCell().ifRoomCenter()) {
-//				movePlayer.move();
-//			}
-//			assertTrue(movePlayer.getCurrentCell().isRoomCenter());
-//		}
-//		
-//		//Check if multiple weaons, rooms and people are in the unseen,
-//		//Then they are selected randomly
-//		List<Card> testReplyList11 		= new Vector<Card>();
-//		List<Card> testReplyList12 		= new Vector<Card>();
-//		List<Card> testReplyList13 		= new Vector<Card>();
-//		Set<Card>	checkSet			= new HashSet<Card>();
-//		ComputerPlayer testPlayer1 = testingPlayerList.get(5);
-//		testPlayer1.
-//		testReplyList11.addAll(testPlayer1.guessPossibleSolutionGetter());
-//		testPlayer1.makeSuggestion();
-//		testReplyList12.addAll(testPlayer1.guessPossibleSolutionGetter());
-//		testPlayer1.makeSuggestion();
-//		testReplyList13.addAll(testPlayer1.guessPossibleSolutionGetter());
-//		checkSet.addAll(testReplyList11);
-//		checkSet.addAll(testReplyList12);
-//		checkSet.addAll(testReplyList13);
-//		
-//		//Remove all cards from all  players hands to ensure that
-//		//The room the accusing player is in will not be refuted
-//		
-//		for(ComputerPlayer removeMyCards: testingPlayerList) {
-//			removeMyCards.emptyHand();
-//			assertTrue(removeMyCards.getHand().size() == 0);
-//		}
-//		
-//		//Knowing the hand all hand sizes are 0, therefore no given cards
-//		//Soo no players can refute any suggestions. Now we check if the Room
-//		//Of the computer player matches the suggested room.
-//		List<Card> testReplyList2 		= new Vector<Card>();
-//		ComputerPlayer testPlayer2 = testingPlayerList.get(0);
-//		testPlayer2.makeSuggestion();
-//		testReplyList2.addAll(testPlayer2.guessPossibleSolutionGetter());
-//		assertTrue(testPlayer2.getCurrentCell().getMyRoomType().getRoomName().contains(testPlayer2.guessPossibleSolutionGetter().get(1).getCardName()));
-//		
-//		//Adding 1 weapon card, and 1 person card to unseen list, removing all others.
-////		testPlayer1.
-//	}
-//	
-//	@Test
-//	public void testOnlyOneCardUnseen() {
-//	
-//	}
-//}	
-//		
+package tests;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import clueGame.*;
+
+public class ComputerAlTest {
+	static Board board;
+	
+	//We'll need these for all the following tests.
+	@BeforeAll
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		// set the file names to use my config files
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");		
+		// Initialize will load config files 
+		board.initialize();
+	}
+	
+	//
+	@Test
+	public void testSuggestedRoomMatchesCurrentLocartion() {
+		Deck testDeck1 = new Deck(ClueFileIO.getFormattedSetupFile());
+		Deck testDeck2 = new Deck(ClueFileIO.getFormattedSetupFile());
+		GlossaryActions.createGlossaryFromDeck(testDeck1);
+		testDeck1 = DeckActions.createSeperateTypeDecks(testDeck1, CardType.PERSON);
+		List<ComputerPlayer> testingPlayerList = new Vector<ComputerPlayer>();	
+		
+		
+		ComputerPlayer testPlayer1 = new ComputerPlayer(testDeck1.getDeck().get(0));
+		ComputerPlayer testPlayer2 = new ComputerPlayer(testDeck1.getDeck().get(1));
+		ComputerPlayer testPlayer3 = new ComputerPlayer(testDeck1.getDeck().get(2));
+		
+		testingPlayerList.add(testPlayer1);
+		testingPlayerList.add(testPlayer2);
+		testingPlayerList.add(testPlayer3);
+			
+		DeckActions.dealDeck(testDeck2, testingPlayerList.stream().collect(Collectors.toList()));
+		testPlayer1.moveMeToCell((Room.roomMap.get('N').getCenterCell()));
+		
+		//Setup a Guess by testPlayer1
+		Guess p1Guess1 = new Guess(testPlayer1.makeSuggestion());
+		//Ensure the guessed Room is the room testPlayer1 is in
+		for(Card checkThisCard: p1Guess1.getGuess()) {
+			if(checkThisCard.getCardtype().equals(CardType.ROOM)) {
+				assertTrue(checkThisCard.getCardName().contains(testPlayer1.getCurrentCell().getMyRoomType().getRoomName()));
+			}
+			
+		}
+		
+		//Move testPLayer1 to a different room, test again
+		testPlayer1.moveMeToCell((Room.roomMap.get('P').getCenterCell()));
+		Guess p1Guess2 = new Guess(testPlayer1.makeSuggestion());
+		//Ensure the guessed Room is the room testPlayer1 is in
+		for(Card checkThisCard: p1Guess2.getGuess()) {
+			if(checkThisCard.getCardtype().equals(CardType.ROOM)) {
+				assertTrue(checkThisCard.getCardName().contains(testPlayer1.getCurrentCell().getMyRoomType().getRoomName()));
+			}
+			
+		}
+		
+		//Move testPlayer1 and ensure the test fails
+		testPlayer1.moveMeToCell((Room.roomMap.get('N').getCenterCell()));
+		for(Card checkThisCard: p1Guess2.getGuess()) {
+			if(checkThisCard.getCardtype().equals(CardType.ROOM)) {
+				assertFalse(checkThisCard.getCardName().contains(testPlayer1.getCurrentCell().getMyRoomType().getRoomName()));
+			}
+			
+		}
+	}
+	
+}	
+		
